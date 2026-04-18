0298173e42fac84202113cddea6ff21c7147efa8
subir301204 <thunderalpha509@gmail.com>
2026-04-18 11:39:06 +0530

    create the basic structure of main file

--- src/main.c
+++ src/main.c
@@ -4,6 +4,16 @@
 
 #include <stdio.h>
 
-int main() {
-  printf("Test");
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "\nERROR: Wrong argument count...\n");
+  }
+
+  /*
+   * There should be a help function to se how can we use the tool
+   * I am thinking to implement that without any flag
+   * You just have to enter the tools name and it will shouw all the functionalities
+   */
+
+  return 0;
 }
