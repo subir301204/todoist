059bcf2db6282b05e9e9a73a291ebed37295d06e
subir301204 <thunderalpha509@gmail.com>
2026-04-18 18:04:11 +0530

    add utility header and c file for utility functions

---
+++ src/utility.c
@@ -0,0 +1,8 @@
+#include "../include/utility.h"
+#include <stdio.h>
+
+void usage() {
+  printf("\nUSAGE: todo <necessary flags>\n");
+  printf("\n----------------Available Flags----------------\n");
+  printf("\ntodo - To get the usage message\n");
+}
