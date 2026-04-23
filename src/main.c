016a260f6364be7482153f7f03e8200ea96f8251
subir301204 <thunderalpha509@gmail.com>
2026-04-24 03:13:03 +0530

    add an error message for no argument setuation

--- src/main.c
+++ src/main.c
@@ -2,11 +2,17 @@
  * This is the initial code for the program
  */
 
+#include <stdio.h>
 #include "../include/flags.h"
 #include "../include/utility.h"
 
 int main(int argc, char *argv[]) {
   Flags fg;
+
+  if (argc == 1) {
+    fprintf(stderr, "\nERROR: We need an argument to perform any operation.\n");
+    return 1;
+  }
 
   init_flags(&fg);
   parse_flags(argc, argv, &fg);
