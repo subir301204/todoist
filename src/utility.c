d0951ce88751e042068baf0ae4683fc95d012186
subir301204 <thunderalpha509@gmail.com>
2026-04-24 00:44:57 +0530

    update usage() (in the utility.c) for the newly available flags

--- src/utility.c
+++ src/utility.c
@@ -4,5 +4,9 @@
 void usage() {
   printf("\nUSAGE: todo <necessary flags>\n");
   printf("\n----------------Available Flags----------------\n");
-  printf("\ntodo - To get the usage message\n");
+  printf("\nc\t-\tCreate new task\n");
+  printf("x\t-\tMark task as complete\n");
+  printf("u\t-\tUpdate any task details\n");
+  printf("d\t-\tDelete a  task\n");
+  printf("todo\n-\nTo get the usage message\n");
 }
