982d320374ffc3e3e76cc709881011b5c3feb8ad
subir301204 <thunderalpha509@gmail.com>
2026-04-24 02:47:33 +0530

    add the implement_operations() fiunction to the utility header and src files

--- include/utility.h
+++ include/utility.h
@@ -1,7 +1,12 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "../include/flags.h"
+
 // Function to show the user manual to the user
 void usage();
+
+// Function to implement the operations according to the flags being used
+void implement_operations(Flags *flags);
 
 #endif
--- src/utility.c
+++ src/utility.c
@@ -1,4 +1,6 @@
 #include "../include/utility.h"
+#include "../include/flags.h"
+#include "../include/task.h"
 #include <stdio.h>
 
 void usage() {
@@ -6,7 +8,27 @@ void usage() {
   printf("\n----------------Available Flags----------------\n");
   printf("\nc\t-\tCreate new task\n");
   printf("x\t-\tMark task as complete\n");
-  printf("u\t-\tUpdate any task details\n");
+  // printf("u\t-\tUpdate any task details\n");
   printf("d\t-\tDelete a  task\n");
   printf("todo\n-\nTo get the usage message\n");
+}
+
+void implement_operations(Flags *flags) {
+  if (flags->create) {
+    // Adding new tasks
+    add_tasks();
+  } else if (flags->mark_as_complete) {
+    int tempID;
+    printf("\nEnter the task ID which to be deleted: ");
+    scanf("%d", &tempID);
+
+    mark_complete(tempID);
+  } else if (flags->list_tasks) {
+    list_tasks();
+  } else if (flags->delete_task) {
+    int tempID;
+    printf("\nEnter the task ID which have to be deleted: ");
+    scanf("%d", &tempID);
+    delete_task(tempID);
+  }
 }
