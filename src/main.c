cb73d4a69e39a4610d9cd36b116d01cae313138f
subir301204 <thunderalpha509@gmail.com>
2026-04-24 11:01:51 +0530

    implement the init_file_path() inside the main function for getting the path globally

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 int main(int argc, char *argv[]) {
   Flags fg;
 
+  init_file_path();
+
   if (argc == 1) {
     fprintf(stderr, "\nERROR: We need an argument to perform any operation.\n");
     return 1;
